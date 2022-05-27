57514424b9a35fd540e63704b27a4ba0e09c5606
kursad karagoz <kursadkaragoz10@gmail.com>
2022-05-27 13:38:06 -0400

    added

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -11,11 +11,12 @@ package javaapplication2;
  */
 public class JavaApplication2 {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World");
     }
     
 }
